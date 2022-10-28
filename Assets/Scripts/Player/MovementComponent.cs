c14b1de9a663689e32afa4d01deb8f74711547f5
Jáchym Bureš <bures.jachym@gmail.com>
2022-10-28 13:30:20 +0200

    Added: PlayerPrototype

--- Assets/Scripts/Player/MovementComponent.cs
+++ Assets/Scripts/Player/MovementComponent.cs
@@ -1,18 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+
 
 public class MovementComponent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [Header("Player Attributes")]
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float speedMultiplier = 1;
+    [SerializeField] private float sensitivity = 1;
+
+    [Header("Player Components")]
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] public InputManager inputManager;
+
+
+
+    [SerializeField] private InputAction moveInput;
+    [SerializeField] private InputAction lookInput;
+
+    [SerializeField] private Vector3 direction;
+    [SerializeField] private float mouseX;
+    [SerializeField] private float mouseY;
+    [SerializeField] private float rotationX;
+    [SerializeField] private float rotationY;
+
+
+    private void OnEnable()
     {
-        
+        inputManager.Enable();
+
+        moveInput = inputManager.Player.Move;
+        moveInput.Enable();
+
+        lookInput = inputManager.Player.Look;
+        lookInput.Enable();
     }
 
-    // Update is called once per frame
+    private void OnDisable()
+    {
+        inputManager.Disable();
+
+        moveInput.Disable();
+        lookInput.Disable();
+    }
+
+
+    void Awake()
+    {
+        inputManager = new InputManager();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
     void Update()
     {
-        
+        direction = transform.right * moveInput.ReadValue<Vector2>().x + transform.forward * moveInput.ReadValue<Vector2>().y;
+        Look();
+    }
+
+    void FixedUpdate()
+    {
+        Move(direction);
+    }
+
+    private void Move(Vector3 _dir)
+    {
+        rb.AddForce(_dir * speed * speedMultiplier, ForceMode.Acceleration);
+    }
+
+    private void Look()
+    {
+        mouseX = lookInput.ReadValue<Vector2>().y;
+        mouseY = lookInput.ReadValue<Vector2>().x;
+
+        rotationX += mouseX * sensitivity * 0.1f;
+        rotationY -= mouseY * sensitivity * 0.1f;
+
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        //cameraT.localRotation = Quaternion.Euler(-rotationX, 0, 0);
+        transform.rotation = Quaternion.Euler(0, -rotationY, 0);
     }
 }
