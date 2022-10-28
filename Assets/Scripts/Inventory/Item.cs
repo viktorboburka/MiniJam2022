97a40eb23e757d5a9fc18bc9dc59fbc6a0a8c1d9
HidekiRozario <TwiDraws@gmail.com>
2022-10-28 12:34:01 +0200

    Prototype Mats

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ItemType {
+    ManualWeapon,
+    AutoWeapon
+}
+
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string itemName;
+    public Sprite itemSprite;
+    public ItemType itemType;
 }
