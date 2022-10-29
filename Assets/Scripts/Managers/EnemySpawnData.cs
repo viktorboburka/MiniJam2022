d3485503aaf2c9fd0c2c74551474c3f1739192bd
Jáchym Bureš <bures.jachym@gmail.com>
2022-10-29 17:18:33 +0200

    Added: More Wave Modifiers

--- Assets/Scripts/Managers/EnemySpawnData.cs
+++ Assets/Scripts/Managers/EnemySpawnData.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemySpawnData", menuName = "ScriptableObjects/WaveManager/EnemySpawnData", order = 1)]
 public class EnemySpawnData : ScriptableObject
 {
+    [Header("Enemy Data")]
     public GameObject enemyPrefab;
+    [Header("Chance To Spawn")]
     public int chanceToSpawn;
-    public int eliteSpawnEachWave = 0;
+    public int modifyChanceToSpawnOn = 2;
+    public int modifyChanceToSpawnBy = 1;
+    [Header("Guaranteed Spawn")]
+    public int guaranteedSpawnOn = 0;
+    public int guaranteedSpawnCount = 1;
+    public int modifyGuaranteedSpawnCountOn = 0;
+    public int modifyGuaranteedSpawnCountBy = 0;
 }
