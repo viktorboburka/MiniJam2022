af30e30512151289bfc8fe9ac0d367e9f1833c70
HidekiRozario <TwiDraws@gmail.com>
2022-10-30 20:32:57 +0100

    Blood blink uwu

--- Assets/BloodSplat.cs
+++ Assets/BloodSplat.cs
@@ -7,9 +7,15 @@ public class BloodSplat : MonoBehaviour
     public float maxScale = 1.2f;
     public float minScale = 0.7f;
     public int damage = 2;
+    public float speedMult = 2f;
+
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
         transform.localScale *= Random.Range(minScale, maxScale);
+        anim = GetComponent<Animator>();
+        anim.speed = 1 + damage * speedMult;
     }
 }
