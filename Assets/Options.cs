a7db25b8506c4504b00140331d68a6f0f3051e84
HidekiRozario <TwiDraws@gmail.com>
2022-10-30 22:56:39 +0100

    Mixers

---
+++ Assets/Options.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    [SerializeField] private AudioMixer masterSFXMixer;
+    [SerializeField] private AudioMixer masterMusicMixer;
+
+    [SerializeField] private Slider masterSFX;
+    [SerializeField] private Slider masterMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        masterSFX.value = PlayerPrefs.GetFloat("SFXVolume");
+        masterMusic.value = PlayerPrefs.GetFloat("MusicVolume");
+        masterSFXMixer.SetFloat("SFXVolume", Mathf.Log10(PlayerPrefs.GetFloat("SFXVolume")) * 20);
+        masterMusicMixer.SetFloat("MusicVolume", Mathf.Log10(PlayerPrefs.GetFloat("MusicVolume")) * 20);
+    }
+
+    public void SetSFX(float value){
+        masterSFXMixer.SetFloat("SFXVolume", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", value);
+    }
+
+    public void SetMusic(float value){
+        masterMusicMixer.SetFloat("MusicVolume", Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", value);
+    }
+}
