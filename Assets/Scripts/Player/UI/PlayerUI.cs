5d789c6c685f1544d1fa72f71b7415a759614822
Dominik Kutynsky <dominikkutynsky@gmail.com>
2022-10-30 03:37:25 +0100

    night

--- Assets/Scripts/Player/UI/PlayerUI.cs
+++ Assets/Scripts/Player/UI/PlayerUI.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class PlayerUI : MonoBehaviour
 {
@@ -14,6 +15,8 @@ public class PlayerUI : MonoBehaviour
     [SerializeField] private Image HpFillBar;
 
 
+    [SerializeField] private InputActionAsset actions;
+
     [SerializeField] private Inventory playerStats;
 
     [SerializeField] private TMP_Text timeText;
@@ -22,14 +25,27 @@ public class PlayerUI : MonoBehaviour
     private Image[] iconImage;
     private RectTransform[] barImage;
     private TMP_Text[] texts;
+    private TMP_Text[] bindTexts;
 
     public void NewSlotWasAdded() {
         var slotCount = AbilitySys.Slots.Count;
+
+        var bindNumber = 1;
 
         for(int i = 0; i < slotCount; i++)
         {
             iconImage[i].color = Color.white;
             iconImage[i].sprite = AbilitySys.Slots[i].item.itemSprite;
+
+            if(AbilitySys.Slots[i].item.itemType == ItemType.ManualWeapon)
+            {
+                var bindKey = actions["Player/Abillity" + bindNumber];
+
+                var displayString = bindKey.GetBindingDisplayString();
+                bindTexts[i].text = displayString;
+                bindNumber++;
+            }
+            
         }
 
     }
@@ -42,11 +58,13 @@ public class PlayerUI : MonoBehaviour
         iconImage = new Image[count];
         texts = new TMP_Text[count];
         barImage = new RectTransform[count];
+        bindTexts = new TMP_Text[count];
         for (int i = 0; i < count; ++i)
         {
             iconImage[i] = itemIcons.transform.GetChild(i).GetComponent<Image>();
             texts[i] = itemIcons.transform.GetChild(i).GetComponentInChildren<TMP_Text>();
             barImage[i] = itemIcons.transform.GetChild(i).Find("Coldown_bar").GetComponent<RectTransform>();
+            bindTexts[i] = itemIcons.transform.GetChild(i).Find("BindBox").GetComponentInChildren<TMP_Text>();
         }
 
     }
@@ -78,7 +96,9 @@ public class PlayerUI : MonoBehaviour
 
         //hp
         float expirience = 1f - ((float)playerStats.GetExperience() / (float)playerStats.GetExperienceNeeded());
-        float hp = 1.06f - ((float)playerStats.GetHP() / (float)playerStats.GetMaxHP());
+        float hp = 1f - ((float)playerStats.GetHP() / (float)playerStats.GetMaxHP());
+        Debug.Log("hp stuff is " + hp);
+
 
         HpFillBar.fillAmount = hp;
         expirienceFillBar.fillAmount = expirience;
