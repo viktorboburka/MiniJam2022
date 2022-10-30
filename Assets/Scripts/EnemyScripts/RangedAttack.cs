dbc8ba8b5e600f8ec0a188d70773027ffea28eb0
Jáchym Bureš <bures.jachym@gmail.com>
2022-10-30 16:35:46 +0100

    Added: Range Elite Changed: XP Drops

--- Assets/Scripts/EnemyScripts/RangedAttack.cs
+++ Assets/Scripts/EnemyScripts/RangedAttack.cs
@@ -6,7 +6,7 @@ public class RangedAttack : Attack
 {
     [SerializeField]
     private float projectileSpeed;
-    [SerializeField] private bool enableProjectilePrediction;
+    [SerializeField] private Transform projectileSpawnT;
     //[SerializeField]
     private float playerMovementPrediction = 1f;
     [SerializeField]
@@ -15,11 +15,23 @@ public class RangedAttack : Attack
     public override void Perform(GameObject player, Enemy enemy) {
         //TODO: play animation & sound
 
-        //playerMovementPrediction = GetDistanceFromPlayer(player) / projectileSpeed;
-        Vector3 projectileTarget = player.transform.position - transform.position; // + player.transform.GetComponent<Rigidbody>().velocity * playerMovementPrediction;
 
-        GameObject projectile = Instantiate(projectilePrefab, this.transform.position, Quaternion.LookRotation(projectileTarget, Vector3.up));
-        projectile.GetComponent<Rigidbody>().velocity = projectile.transform.forward  * projectileSpeed;
+        Vector3 projectileTarget = player.transform.position + player.transform.GetComponent<Rigidbody>().velocity * playerMovementPrediction;
+        
+        Vector3 projectileSpawnPos = this.transform.position;
+        Quaternion projectileSpawnRot = Quaternion.LookRotation(projectileTarget - this.transform.position, Vector3.up);
+        if (projectileSpawnT != null)
+        {
+            projectileSpawnT.LookAt(player.transform);
+            projectileSpawnPos = projectileSpawnT.position;
+            projectileSpawnRot = projectileSpawnT.rotation;
+        }
+
+
+        playerMovementPrediction = GetDistanceFromPlayer(player) / projectileSpeed;
+        GameObject projectile = Instantiate(projectilePrefab, projectileSpawnPos, projectileSpawnRot);
+        projectile.GetComponent<Rigidbody>().velocity = projectileSpawnT.forward * projectileSpeed;
+
         projectile.GetComponent<EnemyProjectile>().info = new AttackInfo(enemy.getDmg(), 0);
     }
 
